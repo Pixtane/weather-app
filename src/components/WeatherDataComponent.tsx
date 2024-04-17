b9ce35f2db5a9732b87b21eb03572b08b8c02340
Pixtane <94226146+Pixtane@users.noreply.github.com>
2024-04-17 22:27:27 +0300

    styling

--- src/components/WeatherDataComponent.tsx
+++ src/components/WeatherDataComponent.tsx
@@ -6,7 +6,9 @@ type Props = {
 
 function WeatherDataComponent(props: Props) {
   let weatherIconsData =
-    weatherIcons[props.weatherData.weather.id as keyof typeof weatherIcons];
+    weatherIcons[props.weatherData.weather[0].id as keyof typeof weatherIcons];
+
+  console.log(props.weatherData.weather.id);
   return (
     <>
       <div className="weatherDataComponent w-full flex flex-col items-center">
@@ -18,9 +20,22 @@ function WeatherDataComponent(props: Props) {
           />
         </div>
 
-        <div className="temperature flex text-8xl font-extrabold mt-16">
-          {Number(props.weatherData.main.temp).toFixed(0)}
-          <p className="tracking-tighter ml-2">°C</p>
+        <div className="temperature mt-16 flex flex-col items-center">
+          <div className="flex text-9xl font-normal">
+            {Number(props.weatherData.main.temp).toFixed(0)}
+            <p className="tracking-tighter text-5xl ml-0">°C</p>
+          </div>
+
+          <div className="flex flex-row text-gray-50 items-center text-3xl">
+            {props.weatherData.weather[0].main}{" "}
+            <p className="flex ml-3">
+              {Number(props.weatherData.main.temp_min).toFixed(0)}
+              <p className="tracking-tighter ml-0.5">°C</p>
+              <p className="text-gray-300">/</p>
+              {Number(props.weatherData.main.temp_max).toFixed(0)}
+              <p className="tracking-tighter ml-0.5">°C</p>
+            </p>
+          </div>
         </div>
       </div>
     </>
