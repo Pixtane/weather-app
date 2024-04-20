f1808d46dc7771d5986d909346bf8015a8f08025
Pixtane <94226146+Pixtane@users.noreply.github.com>
2024-04-20 10:02:08 +0300

    remove unused

--- src/components/PlaceSearchComponent.tsx
+++ src/components/PlaceSearchComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import countrycodes from "../assets/countrycodes.json";
 interface Place {
