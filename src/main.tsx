d8e96b31bc0983b094c3945cb41a06adc91f67b0
Pixtane <94226146+Pixtane@users.noreply.github.com>
2024-05-11 10:43:20 +0300

    1.3.3 Remove StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,4 @@ import App from "./App.tsx";
 import "./index.css";
 import "../public/fonts/Lexend_Deca/lexend_deca.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
